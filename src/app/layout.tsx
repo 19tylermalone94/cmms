21f9c2865b058e3a0ff93d48cedb39983be3ac21
Tyler <19tylermalone94@gmail.com>
2024-12-19 19:44:51 -0700

    fix tailwind config files to get style working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import './globals.css';
+
+export const metadata = {
+  title: 'CMMS',
+  description: 'computerized maintenance management system',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
