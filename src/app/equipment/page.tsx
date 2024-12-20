3d2f4d69b9e930042af6610bf03ce5dcbbbeb0f8
Tyler <19tylermalone94@gmail.com>
2024-12-19 19:25:07 -0700

    create equipment route

---
+++ src/app/equipment/page.tsx
@@ -0,0 +1,10 @@
+import EquipmentForm from "../components/EquipmentForm";
+
+export default function Equipment() {
+  return (
+    <div>
+      <h1>Add New Equipment</h1>
+      <EquipmentForm></EquipmentForm>
+    </div>
+  );
+}
