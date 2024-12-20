e36e6bc6913d1b10167605c509423c843b313e38
Tyler <19tylermalone94@gmail.com>
2024-12-19 19:29:42 -0700

    add link on equipment page to home route

--- src/app/equipment/page.tsx
+++ src/app/equipment/page.tsx
@@ -1,10 +1,20 @@
+import Link from "next/link";
 import EquipmentForm from "../components/EquipmentForm";
 
 export default function Equipment() {
   return (
     <div>
-      <h1>Add New Equipment</h1>
-      <EquipmentForm></EquipmentForm>
+      <div>
+        <h1>Add New Equipment</h1>
+        <EquipmentForm></EquipmentForm>
+      </div>
+      <div>
+      <Link href="/">
+        <button className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
+          Back to dashboard
+        </button>
+      </Link>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">CMMS</h1>
-      
+      <h1 className="text-4xl font-bold mb-6">Dashboard</h1>
       <Link href="/equipment">
         <button className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
           Add new equipment
