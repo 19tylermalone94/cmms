4bd0d4dd27ee56f6b2f5886bec579d31116a1a3d
Tyler <19tylermalone94@gmail.com>
2024-12-21 12:14:16 -0700

    rename maint action

--- src/app/actions/addMaintenanceRecord.ts
+++ src/app/actions/addMaintenanceRecord.ts
@@ -2,7 +2,7 @@
 
 import { v4 as uuidv4 } from 'uuid';
  
-export async function createEquipment(data: Omit<MaintenanceRecord, "id">) {
+export async function createMaintenanceRecord(data: Omit<MaintenanceRecord, "id">) {
   const id = uuidv4();
   const newMaintenanceRecord = {...data, id}
   // add equipment to db from here
