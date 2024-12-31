d97583d3fa4aafbc6555e693d624e1baf9e3bbe0
Tyler <19tylermalone94@gmail.com>
2024-12-31 01:21:41 -0700

    add link to add equipment from equipment directory

--- src/app/equipment/page.tsx
+++ src/app/equipment/page.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
-import EquipmentForm from "../components/EquipmentForm";
 import EquipmentTable from "../components/EquipmentTable";
 
 export default function Equipment() {
   return (
     <div>
       <div>
-        <h1>Add New Equipment</h1>
-        <EquipmentForm></EquipmentForm>
+        <h1>Equipment</h1>
+        <EquipmentTable />
+      </div>
+      <div>
+      <Link href="/equipment/add">
+        <button className="px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600">
+          Add New Equipment
+        </button>
+      </Link>
       </div>
       <div>
       <Link href="/">
